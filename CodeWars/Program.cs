451d0b9f8648a77604b5e729aa02af4a1cbaa59d
alochimpasplum <lagoscarrera@gmail.com>
2020-10-14 14:37:52 +0200

    Kiu6 : Find the odd int

--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -11,8 +11,7 @@ namespace CodeWars
     {
         static void Main()
         {
-            GetSet.testBool = false;
-            Console.WriteLine(GetSet.testBool);
+            Console.WriteLine(kiu6.FindTheOddInt.find_it(new[] { 20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5 }));
             Console.ReadKey();
         }
     }
---
+++ CodeWars/kiu6/FindTheOddInt.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.kiu6
+{
+    static class FindTheOddInt
+    {
+        private static List <Number> Numbers { get; set; }
+        public static int find_it(int[] seq)
+        {
+            Numbers = new List<Number>();
+            int number = 0;
+
+            foreach(int i in seq)
+            {
+                if (Numbers.Any(x => x.index == i))
+                {
+                    Numbers.Find(x => x.index == i).repeats++;
+                } else
+                {
+                    Numbers.Add(new Number(i));
+                }
+            }
+            foreach(Number n in Numbers)
+            {
+                if (n.repeats % 2 != 0) { number = n.index; }
+            }
+            return number;
+        }
+
+        public static int betterSol(int[] seq)
+        {
+            return seq.GroupBy(x => x).Single(g => g.Count() % 2 == 1).Key;
+        }
+    }
+
+    class Number
+    {
+        public int index { get; set; }
+        public int repeats { get; set; }
+        public Number(int number)
+        {
+            index = number;
+            repeats = 1;
+        }
+    }
+}
