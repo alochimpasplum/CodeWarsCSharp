4315e0aaf559466e57f299b3c80b20ff8f9ffe77
alochimpasplum <lagoscarrera@gmail.com>
2020-10-29 07:06:00 +0100

    PersistentBugger

--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -11,7 +11,7 @@ namespace CodeWars
     {
         static void Main()
         {
-            Console.WriteLine(kiu6.FindTheOddInt.find_it(new[] { 20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5 }));
+            Console.WriteLine(kiu6.PersistentBugger.Persistence(3));
             Console.ReadKey();
         }
     }
---
+++ CodeWars/kiu6/PersistentBugger.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.kiu6
+{
+    public static class PersistentBugger
+    {
+        public static int Persistence(long n)
+        {
+            int i = 0;
+            string number = n.ToString();
+
+            while (number.ToString().Length != 1)
+            {
+                long numTemp = 1;
+
+                foreach (char c in number)
+                {
+                    numTemp = numTemp * long.Parse(c.ToString());
+                }
+
+                number = numTemp.ToString();
+
+                i++;
+            }
+
+            return i;
+        }
+
+        public static int PersistenceLinq(long n)
+        {
+            int count = 0;
+            while (n > 9)
+            {
+                count++;
+                n = n.ToString().Select(digit => int.Parse(digit.ToString())).Aggregate((x, y) => x * y);
+            }
+            return count;
+        }
+    }
+}
